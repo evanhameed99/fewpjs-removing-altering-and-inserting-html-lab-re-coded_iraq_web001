ee567f231d243daf8b03133f6fcf5355b425b8d8
Evan hameed <evanhameed9966@gmail.com>
2020-05-13 10:00:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 // Write your code here!
  let main = document.querySelector('main#main');
  main.remove();
-
-let newHeader  = document.querySelector('h1#victory').innerHTML= "Evan is the champion";
